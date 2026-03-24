facc59623633d7fdf94429b3b128c88b9e0840c5
Madusha Damith <madushdamith8@gmail.com>
2026-03-24 20:03:55 +0530

    Removed comments

--- Ongoing_Counter_Stack/missing.h
+++ Ongoing_Counter_Stack/missing.h
@@ -3,13 +3,13 @@
 
 #include "../Token_Generator/token.h"
 
-// Circular linked list of missing tokens
+
 struct Missing_list {
     struct token *head;
     struct token *tail;
 };
 
-// Function declarations
+
 void init_Missing_list(struct Missing_list *list);
 void insert(struct Missing_list *list, struct token* token);
 void delete_id(struct Missing_list *list, struct token* token);
